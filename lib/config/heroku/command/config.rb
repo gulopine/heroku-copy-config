4d09a058dc7887583563d72064415b23f5cad197
scottpersinger <scottp@heroku.com>
2014-03-20 13:48:14 -0700

    First commit

---
+++ lib/config/heroku/command/config.rb
@@ -0,0 +1,83 @@
+require "heroku/command/config"
+
+class Heroku::Command::Config
+
+  # config:copy
+  #
+  # Copy one or more config vars from one app to another
+  #
+  def copy
+    unless args.size > 1
+      error("Usage: heroku config:copy <target app name> KEY1 [KEY2...]")
+    end
+    target_app = args.shift
+    vars = api.get_config_vars(app).body.select {|key, value| args.include?(key.to_s)}
+    puts vars.inspect
+  end
+
+  # config:push
+  #
+  # push local config vars to heroku
+  #
+  # will not overwrite existing config vars by default
+  #
+  # -i, --interactive  # prompt whether to overwrite each config var
+  # -o, --overwrite    # overwrite existing config vars
+  #
+  def push
+    interactive = options[:interactive]
+    overwrite   = options[:overwrite]
+
+    config = merge_config(local_config, remote_config, interactive, overwrite)
+    write_remote_config config
+    display "Config in .env written to #{app}"
+  end
+
+private ######################################################################
+
+  def local_config
+    File.read(".env").split("\n").inject({}) do |hash, line|
+      if line =~ /\A([A-Za-z0-9_]+)=(.*)\z/
+        hash[$1] = $2
+      end
+      hash
+    end
+  rescue
+    {}
+  end
+
+  def remote_config
+    api.get_config_vars(app).body
+  end
+
+  def write_local_config(config)
+    File.open(".env", "w") do |file|
+      config.keys.sort.each do |key|
+        file.puts "#{key}=#{config[key]}"
+      end
+    end
+  end
+
+  def write_remote_config(config)
+    add_config_vars = config.inject({}) do |hash, (key,val)|
+      hash[key] = val unless remote_config[key] == val
+      hash
+    end
+
+    api.put_config_vars(app, add_config_vars)
+  end
+
+  def merge_config(source, target, interactive=false, overwrite=false)
+    if interactive
+      source.keys.sort.inject(target) do |hash, key|
+        value = source[key]
+        display "%s: %s" % [key, value]
+        hash[key] = value if confirm("Overwite? (y/N)")
+        hash
+      end
+    else
+      overwrite ? target.merge(source) : source.merge(target)
+    end
+  end
+
+end
